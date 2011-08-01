26800c91121e7b7082db1947bdb8b355490b2848
endungutse <ndemcassius@gmail.com>
2011-08-01 15:48:16 -0500

    update system_test

--- system_test.sh
+++ system_test.sh
@@ -1,13 +1,17 @@
+
 #!/bin/bash
 
-server=${1-localhost:8085}
+server=${1-localhost:8085} #take the first parameter
+ 
+curl -s -H Content-Type:application/json -d '["one"]' http://$server/checkclearing
 
-response= `curl -s -H Content-Type:application/json -d '["one"]' http://$server/checkclearing`
-if [ $response = '{"one":100}' ]; then
-	#echo $response
-	echo "TEST succeeded!!!!!!!!!!!!!!"
-	exit 1
+history=`curl http://$server/checkclearing`
+response=`curl -s -H Content-Type:application/json -d "$history" http://$server/checkclearing`
+
+if [ "$response" != '{"one":100}' ]; then
+echo $response
+echo "TEST FAILED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+exit 1
 else
-	echo "TEST failed"
+echo "Test succeeded"
 fi
-
