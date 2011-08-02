232b88174347d5ebc067305ee6163cb2d9c87bd8
endungutse <ndemcassius@gmail.com>
2011-08-02 13:33:49 -0500

    update

--- system_test.sh
+++ system_test.sh
@@ -3,12 +3,12 @@
 
 server=${1-localhost:8085} #take the first parameter
  
-curl -s -H Content-Type:application/json -d '["one"]' http://$server/checkclearing
+curl -s -H Content-Type:application/json -d '["two million three hundred fourty six thousand + two dollars and 10/100"]' http://$server/checkclearing
 
 history=`curl http://$server/checkclearing`
 response=`curl -s -H Content-Type:application/json -d "$history" http://$server/checkclearing`
 
-if [ "$response" != '{"one":100}' ]; then
+if [ "$response" != '{"two million three hundred fourty six thousand + two dollars and 10/100"]":234600210}' ]; then
 echo $response
 echo "TEST FAILED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 exit 1
