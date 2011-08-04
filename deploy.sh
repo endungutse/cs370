d1fe129c65fece9c295ab64a022acc19e0c0d455
endungutse <ndemcassius@gmail.com>
2011-08-04 13:37:21 -0500

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -50,7 +50,7 @@ echo -n
   sleep 1
 done
 
-history=`curl http://cbetheridge-cs370.appspot.com/checkclearing`
+history=`curl http://ndemcassius.appspot.com/checkclearing`
 response=`curl -s -H Content-Type:application/json -d "$history" http://localhost:8085/checkclearing`
 
 echo $response | python -mjson.tool > /dev/null
